7c0c75007bfa277a83859d9fe3fc9c5d12e63441
Germán Guerrero <87212199+GermanGuerrero95@users.noreply.github.com>
2021-08-10 01:57:29 -0500

    Add files via upload

---
+++ Formulariobar/script.js
@@ -0,0 +1,12 @@
+let Nombre =document.getElementById("name")
+let Apellidos =document.getElementById("lastname")
+let Edad =document.getElementById("age")
+let RH =document.getElementById("RH")
+let Tel =document.getElementById("Tel")
+
+if (Nombre = true) {
+    document.getElementById('boton1').onclick = function(){
+        alert('Carnet Generado');
+    }
+    
+} 
